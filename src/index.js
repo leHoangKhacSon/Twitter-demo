2cf6e55ab5e7f575e3140fa4bdc296a78f29c0f0
leHoangKhacSon <sonqueen.no1@gmail.com>
2019-11-04 16:07:09 +0700

    Update twitter demo v.2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import autosize from 'autosize';
     const ta = document.querySelector('textarea');
+    const showPost = document.getElementById('textarea');
+    autosize(showPost);
     autosize(ta);
     
+
